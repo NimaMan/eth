fb470c59a8c18ed29b35ad33691b559441ff4cfb
Nima <nima.manaf8@gmail.com>
2025-03-27 20:07:24 +0100

    Implement transaction status check in LiveERC20Token update methods
    - Add early return for unsuccessful transactions in both synchronous and asynchronous update_from_transaction methods to prevent unnecessary processing.
    - This change serves the objective of enhancing the efficiency and reliability of token updates by ensuring that only valid transactions are processed, thereby improving overall system performance.

--- pyeth/eth_token/eth_token/live_erc20_token/live_token.py
+++ pyeth/eth_token/eth_token/live_erc20_token/live_token.py
@@ -25,11 +25,15 @@ class LiveERC20Token:
         self.token_health_predictor = TokenHealthPredictor()
 
     def update_from_transaction(self, transaction: Dict):
+            if not transaction["status"]:
+                return
             self.token_data.update_from_transaction(transaction)
             self.token_network.update_from_transaction(transaction)
             self.latest_token_assessment = self.token_health_predictor.update_from_transaction(transaction, self)
 
     async def update_from_transaction_async(self, transaction: Dict):
+            if not transaction["status"]:
+                return
             self.token_data.update_from_transaction(transaction)
             self.token_network.update_from_transaction(transaction)
             self.latest_token_assessment = self.token_health_predictor.update_from_transaction(transaction, self)
