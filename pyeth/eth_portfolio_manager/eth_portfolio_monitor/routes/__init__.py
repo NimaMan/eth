6c9496f1258ecd3eee6d6310f2f9f6b2e34a95ec
Nima <nima.manaf8@gmail.com>
2026-05-08 13:07:05 +0200

    Remove legacy ETH portfolio monitor

--- pyeth/eth_portfolio_manager/eth_portfolio_monitor/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .live_position_api_routes import live_position_api
-from .main_routes import main
-from .backtest_api_routes import backtest_api
-
-
-__all__ = ['live_position_api', 'main', 'backtest_api']
--- pyeth/eth_portfolio_manager/eth_portfolio_monitor/routes/live_position_api_routes.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-
-live_position_api = Blueprint('live_position_api', __name__)
-
